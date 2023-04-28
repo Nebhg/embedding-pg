36de95856a548e97ba73de62dca836390f3dc1ac
Nebhg <benharagummer@gmail.com>
2023-04-28 22:16:26 +0100

    Sucessfully tokenised and chunked my recipe data

--- scripts/scrape.ts
+++ scripts/scrape.ts
@@ -1,180 +1,91 @@
-import axios from 'axios'
-import * as cheerio from 'cheerio'
-import {encode} from 'gpt-3-encoder'
-import {PGEssay, PGChunk, PGJSON} from '../types/index'
-import fs from 'fs'
+import { encode } from "gpt-3-encoder";
+import fs from "fs";
 
-const BASE_URL = "http://www.paulgraham.com";
-const CHUNK_SIZE = 200;
-
-const getLinks = async () => {
-    const html = await axios.get(`${BASE_URL}/articles.html`);
-    const $ = cheerio.load(html.data);
-    //console.log(html);
-
-    const tables = $("table");
-    //create an array of objects with the url and title of each link
-    const linkArr: {url: string, title: string}[] = [];
-
-    tables.each((i, table) => {
-        if(i == 2) {
-            const links = $(table).find("a")
-            
-            links.each((i, link) => {
-                const url = $(link).attr("href")
-                const title = $(link).text()
-
-                if(url && url.endsWith(".html") && title) {
-                    const linkObj = {
-                        url,
-                        title
-                    }
-                    linkArr.push(linkObj)
-                }
-        })
-        }
-    })
-
-    return linkArr;
+type Recipe = {
+  title: string;
+  total_time: number;
+  yields: string;
+  ingredients: string[];
+  nutrition: {
+    calories: string;
+    carbohydrateContent: string;
+    cholesterolContent: string;
+    fiberContent: string;
+    proteinContent: string;
+    saturatedFatContent: string;
+    sodiumContent: string;
+    sugarContent: string;
+    fatContent: string;
+    unsaturatedFatContent: string;
+  };
+  instructions_list: string[];
+  image: string;
 };
 
-const getEssay = async (url: string, title: string ) => {
-  
-    let essay: PGEssay = {
-        title,
-        url,
-        date: "",
-        content: "",
-        tokens: 0,
-        chunks: []
-    }
-
-    const html = await axios.get(`${BASE_URL}/${url}`);
-    
-    const $ = cheerio.load(html.data);
-    
-    const tables = $("table");
-    
-    
-    tables.each((i, table) => {
-        if(i == 1) {
-                const text = $(table).text();
-
-                let cleanedText = text.replace(/\s+/g, " ").replace(/\.([a-zA-Z])/g, ". $1");
-
-                const split = cleanedText.match(/([A-Z][a-z]+ [0-9]{4})/);
-                let dateStr = "";
-                let textWithoutDate = "";
-                
-                if(split) {
-                    dateStr = split[0];
-                    textWithoutDate = cleanedText.replace(dateStr, "");
-                }
-
-                let essayText = textWithoutDate.replace(/\n/g, " ").trim();
-
-                essay = {
-                    title,
-                    url: `${BASE_URL}/${url}`,
-                    date: dateStr,
-                    content: essayText,
-                    tokens: encode(essayText).length,
-                    chunks: []
-                }
-
-            }
-            });
-            return essay;
-
+type Chunk = {
+  recipe_title: string;
+  content: string;
+  content_tokens: number;
+  embedding: any[];
 };
 
-const getChunks = async (essay: PGEssay) => {
-    const {title, url, date, content} = essay;
+type ChunkedRecipe = Recipe & {
+  chunks: Chunk[];
+};
 
-    let essayTextChunks: string[] = [];
+const getChunks = async (recipe: Recipe): Promise<ChunkedRecipe> => {
+  const { title, total_time, yields, ingredients, nutrition, instructions_list, image } = recipe;
 
-    if(encode(content).length > CHUNK_SIZE){
-        const split = content.split(". ");  //split the essay into sentences
-        let chunkText = "";
+  // Concatenate all the data into a single string
+  const content = `${title}\n\nTotal Time: ${total_time} minutes\nYields: ${yields}\n\nIngredients:\n${ingredients.join(
+    "\n"
+  )}\n\nInstructions:\n${instructions_list.join("\n")}\n\nNutrition:\nCalories: ${nutrition.calories}\nCarbohydrate Content: ${
+    nutrition.carbohydrateContent
+  }\nCholesterol Content: ${nutrition.cholesterolContent}\nFiber Content: ${nutrition.fiberContent}\nProtein Content: ${
+    nutrition.proteinContent
+  }\nSaturated Fat Content: ${nutrition.saturatedFatContent}\nSodium Content: ${nutrition.sodiumContent}\nSugar Content: ${
+    nutrition.sugarContent
+  }\nFat Content: ${nutrition.fatContent}\nUnsaturated Fat Content: ${nutrition.unsaturatedFatContent}\n\nImage: ${image}`;
 
-        for(let i = 0; i < split.length; i++) {
-            const sentence = split[i];
-            const sentenceTokenLength = encode(sentence).length;
-            const chunkTextTokenLength = encode(chunkText).length;
+  const chunk: Chunk = {
+    recipe_title: title,
+    content,
+    content_tokens: encode(content).length,
+    embedding: [],
+  };
 
-            if(chunkTextTokenLength + sentenceTokenLength > CHUNK_SIZE) {
-                essayTextChunks.push(chunkText);
-                chunkText = "";
+  const chunkedRecipe: ChunkedRecipe = {
+    ...recipe,
+    chunks: [chunk],
+  };
 
-            }
-            if(sentence[sentence.length - 1].match(/[a-z0-9]/i)) {
-                chunkText += sentence + ". ";
-            } else {
-                chunkText += sentence + " ";
-            }
-        }
-        essayTextChunks.push(chunkText.trim());
-    }
-    else {
-        essayTextChunks.push(content);
-    }
+  return chunkedRecipe;
+};
 
-    const essayChunks: PGChunk[] = essayTextChunks.map((chunkText, i) => {
-        const chunk: PGChunk = {
-            essay_title: title,
-            essay_url: url,
-            essay_date: date,
-            content: chunkText,
-            content_tokens: encode(chunkText).length,
-            embedding: []
-        };
-        return chunk;
-    });
 
-    if(essayChunks.length > 1) {
-     for(let i = 0; i < essayChunks.length; i++) {
-         const chunk = essayChunks[i];
-         const prevChunk = essayChunks[i - 1];
+const printRandomRecipe = (chunkedRecipes: ChunkedRecipe[]): void => {
+  const randomIndex = Math.floor(Math.random() * chunkedRecipes.length);
+  const randomRecipe = chunkedRecipes[randomIndex];
+  const randomRecipeContent = randomRecipe.chunks[0].content;
 
-         if(chunk.content_tokens < 100 && prevChunk) {
-            prevChunk.content += " " + chunk.content;
-            prevChunk.content_tokens = encode(prevChunk.content).length;
-            essayChunks.splice(i, 1);
-         }
-         
-     }
-    }
-    const chunkedEssay: PGEssay = {
-        ...essay,
-        chunks: essayChunks
-    }
-    return chunkedEssay;
-}
+  console.log(randomRecipeContent);
+  console.log("\nToken Count:", randomRecipe.chunks[0].content_tokens);
+};
 
-( async () => {
-    const links = await getLinks();
-    //console.log(links);
 
-    let essays: PGEssay[] = [];
+(async () => {
+  const recipeDataPath = "scripts/recipes.json";
+  const recipeData: Recipe[] = JSON.parse(fs.readFileSync(recipeDataPath, "utf-8"));
+  let chunkedRecipes: ChunkedRecipe[] = [];
 
-    for (let i = 0; i < links.length; i++) {
-        
-        const link = links[i];
-        //console.log(link);
-        
-        //if(i !== 0) break;
-        const essay = await getEssay(link.url, link.title);
-        const chunkedEssay = await getChunks(essay);
-        //console.log(essay);
-        essays.push(chunkedEssay);
-        console.log(chunkedEssay);
-        
-    }
-    //console.log(links);
-    const json: PGJSON = {
-        tokens: essays.reduce((acc, essay) => acc + essay.tokens, 0),
-        essays
-    }
+  for (const recipe of recipeData) {
+    const chunkedRecipe = await getChunks(recipe);
+    chunkedRecipes.push(chunkedRecipe);
+  }
 
-    fs.writeFileSync("scripts/pg.json", JSON.stringify(json));
-}) ();
+  const chunkedRecipesPath = "scripts/chunkedRecipes.json";
+  fs.writeFileSync(chunkedRecipesPath, JSON.stringify(chunkedRecipes));
+
+  // Print a random recipe from the chunkedRecipes array
+  printRandomRecipe(chunkedRecipes);
+})();
